a0e2830e08d6ad34eff098a1588a357b4d228276
janakhpon <jnovaxer@gmail.com>
2019-09-01 15:48:44 +0630

    GDEV004

--- client/src/Components/Notes/index.js
+++ client/src/Components/Notes/index.js
@@ -1,65 +1,81 @@
 import React, {
   Component
 } from 'react';
-import {  Container, Header, List } from 'semantic-ui-react';
+import {  Container, Button, Checkbox, Icon, Table  } from 'semantic-ui-react';
 
 
 export default class ListForm extends Component {
   render(){
     return(
       <Container text>
-          <Header as='h2'>Header</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-              magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-              quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-              arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-              Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-              dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-              Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-              Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-              viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-              Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-              magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-              quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-              arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-              Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-              dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-              Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-              Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-              viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-              Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-            </p>
-              <List divided relaxed>
-                <List.Item>
-                  <List.Icon name='github' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
-                    <List.Description as='a'>Updated 10 mins ago</List.Description>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
-                  <List.Icon name='github' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>Semantic-Org/Semantic-UI-Docs</List.Header>
-                    <List.Description as='a'>Updated 22 mins ago</List.Description>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
-                  <List.Icon name='github' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>Semantic-Org/Semantic-UI-Meteor</List.Header>
-                    <List.Description as='a'>Updated 34 mins ago</List.Description>
-                  </List.Content>
-                </List.Item>
-              </List>
+        <hr/>
+        <hr/>
+            <Table compact celled definition>
+                 <Table.Header>
+                   <Table.Row>
+                     <Table.HeaderCell />
+                     <Table.HeaderCell>Name</Table.HeaderCell>
+                     <Table.HeaderCell>Registration Date</Table.HeaderCell>
+                     <Table.HeaderCell>E-mail address</Table.HeaderCell>
+                     <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                   </Table.Row>
+                 </Table.Header>
+
+                 <Table.Body>
+                   <Table.Row>
+                     <Table.Cell collapsing>
+                       <Checkbox slider />
+                     </Table.Cell>
+                     <Table.Cell>John Lilki</Table.Cell>
+                     <Table.Cell>September 14, 2013</Table.Cell>
+                     <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+                     <Table.Cell>No</Table.Cell>
+                   </Table.Row>
+                   <Table.Row>
+                     <Table.Cell collapsing>
+                       <Checkbox slider />
+                     </Table.Cell>
+                     <Table.Cell>Jamie Harington</Table.Cell>
+                     <Table.Cell>January 11, 2014</Table.Cell>
+                     <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+                     <Table.Cell>Yes</Table.Cell>
+                   </Table.Row>
+                   <Table.Row>
+                     <Table.Cell collapsing>
+                       <Checkbox slider />
+                     </Table.Cell>
+                     <Table.Cell>Jill Lewis</Table.Cell>
+                     <Table.Cell>May 11, 2014</Table.Cell>
+                     <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+                     <Table.Cell>
+
+                       <Button primary size='small'>Approve</Button>
+
+                     </Table.Cell>
+                   </Table.Row>
+                 </Table.Body>
+
+                 <Table.Footer fullWidth>
+                   <Table.Row>
+                     <Table.HeaderCell />
+                     <Table.HeaderCell colSpan='4'>
+                       <Button
+                         floated='right'
+                         icon
+                         labelPosition='left'
+                         primary
+                         size='small'
+                       >
+                         <Icon name='user' /> Add User
+                       </Button>
+                       <Button size='small'>Approve</Button>
+                       <Button disabled size='small'>
+                         Approve All
+                       </Button>
+                     </Table.HeaderCell>
+                   </Table.Row>
+                 </Table.Footer>
+                </Table>
         </Container>
 
     );
